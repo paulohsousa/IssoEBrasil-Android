0096d42b069a097fe5cc28764cee3110040d077b
victormf123 <victormf.df@hotmail.com>
2018-04-10 23:12:42 -0300

    first commit

---
+++ app/src/main/java/ieb/coffee/com/br/issoebrasil/MainActivity.java
@@ -0,0 +1,17 @@
+package ieb.coffee.com.br.issoebrasil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
